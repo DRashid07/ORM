e0585f0624453abe4753410c04d5805435a97c59
Rashid E. Dursunov <rashided@code.local>
2026-01-04 14:36:25 +0400

    first commit

---
+++ OrmPractice/Data/AppDbContext.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.EntityFrameworkCore;
+using OrmPractice.Models;
+
+namespace OrmPractice.Data
+{
+    internal class AppDbContext : DbContext
+    {
+        public DbSet<Student> Students { get; set; }
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            optionsBuilder.UseSqlServer("Server=SUN06\\MAIN;Database=OrmPracticeDb;Trusted_Connection=True;TrustServerCertificate=True;");
+            base.OnConfiguring(optionsBuilder);
+        }
+    }
+}
---
+++ OrmPractice/Program.cs
@@ -0,0 +1,51 @@
+﻿namespace OrmPractice
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            
+            using (var coxtext = new Data.AppDbContext())
+            {
+                var newStudent = new Models.Student
+                {
+                    Name = "John Doe",
+                    Description = "A new student",
+                    Age = 20
+                };  
+                coxtext.Students.Add(newStudent);
+                coxtext.SaveChanges();
+            }
+            using (var context = new Data.AppDbContext())
+            {
+                var student = context.Students.FirstOrDefault(s => s.Name == "John Doe");
+                if (student != null)
+                {
+                    student.Age = 21;
+                    context.SaveChanges();
+                }
+            }
+            using (var context = new Data.AppDbContext())
+            {
+                var student = context.Students.FirstOrDefault(s => s.Name == "John Doe");
+                if (student != null)
+                {
+                    context.Students.Remove(student);
+                    context.SaveChanges();
+                }
+            }
+            using (var context = new Data.AppDbContext())
+            {
+                var student = context.Students.Where(context => context.Age > 21 && context.Contains("a").ToList());
+            }
+            using (var context = new Data.AppDbContext())
+            {
+                var students = context.Students.ToList();
+                foreach (var student in students)
+                {
+                    Console.WriteLine($"Id: {student.Id}, Name: {student.Name}, Description: {student.Description}, Age: {student.Age}");
+                }
+            }
+        }
+    }
+}
